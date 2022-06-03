99b5d7622840018bad1934c06375c8a0bbe6d8c3
Jonsey <mwjones2186@gmail.com>
2022-06-02 21:17:25 -0700

    files created and moment.js used to add current date and time to top of page.

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+//AS AN employee with a busy schedule
+//I WANT to add important events to a daily planner
+//SO THAT I can manage my time effectively
+
+// //GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours
+// WHEN I view the time blocks for that day
+// THEN I can enter an event
+
+
+// current day is displayed at the top of the calendar
+//use moment.js
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+
+
+
+// WHEN I refresh the page
+// THEN the saved events persist
+
+
+
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// WHEN I click into a time block
