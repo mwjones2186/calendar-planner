b4312240c75d1efddef9f1a6a3c791c1d3cf0a5e
Jonsey <mwjones2186@gmail.com>
2022-06-08 21:44:05 -0700

    added local stoarge blocks to JS. Functioning at this point. Need to add the format time function

--- assets/script.js
+++ assets/script.js
@@ -37,7 +37,21 @@ $('.saveBtn').on('click', function(){
 //need to identify how to grab date from local storage. Use getItem function. 
 //Need to target parent child and want thier value. 
 $("#4 .description").val(localStorage.getItem("4"))
-
+$("#5 .description").val(localStorage.getItem("5"))
+$("#6 .description").val(localStorage.getItem("6"))
+$("#7 .description").val(localStorage.getItem("7"))
+$("#8 .description").val(localStorage.getItem("8"))
+$("#9 .description").val(localStorage.getItem("9"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
+$("#18 .description").val(localStorage.getItem("18"))
+$("#19 .description").val(localStorage.getItem("19"))
+$("#20 .description").val(localStorage.getItem("20"))
 
 //      THEN each time block is color-coded to indicate whether it is in the past, present, or future
 //      WHEN I click into a time block
